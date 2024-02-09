e85c86dc9617d8075741656efe0239cc27f90016
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-02-09 14:17:46 +0200

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('Form');
+'use strict';
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const textarea = form.elements.message;
+const localStorageKey = 'feedback-form-state';
+
+function inputsPageReboot() {
+  const localStorageValue = JSON.parse(localStorage.getItem(localStorageKey));
+  if (localStorageValue) {
+    emailInput.value = localStorageValue.email;
+    textarea.value = localStorageValue.message;
+  }
+}
+inputsPageReboot();
+
+form.addEventListener('input', () => {
+  const inputsValues = {
+    email: emailInput.value.trim(),
+    message: textarea.value.trim(),
+  };
+
+  localStorage.setItem(localStorageKey, JSON.stringify(inputsValues));
+});
+
+form.addEventListener('submit', event => {
+  if (emailInput.value === '' || textarea.value === '') {
+    return alert('All form fields must be filled in');
+  }
+  event.preventDefault();
+  console.log(`Email: ${emailInput.value.trim()}`);
+  console.log(`Message: ${textarea.value.trim()}`);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
